b5b08d9cb81c13cc1d35e39c5146fa7e13218562
HenriqueRamos13 <henrique.mrcr@gmail.com>
2025-12-02 20:51:05 +0000

    test

--- dividir.py
+++ dividir.py
@@ -1,7 +1,7 @@
 def dividir(numerador, denominador):
     if denominador == 0:
         raise ZeroDivisionError("division by zero")
-    return numerador / denominador
+    return numerador * denominador
 
 
 if __name__ == "__main__":
