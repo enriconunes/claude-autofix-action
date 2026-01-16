d741c40d857ce36cf9b08c2651be427de22116d4
Enrico Nunes <enriconunes02@hotmail.com>
2026-01-16 11:48:18 +0000

    novo erro

--- dividir.py
+++ dividir.py
@@ -1,9 +1,7 @@
 def dividir(numerador, denominador):
     if denominador == 0:
         raise ZeroDivisionError("division by zero")
-    return numerador * denominador
-
-    
+    return numerador - denominador
 
 if __name__ == "__main__":
     resultado = dividir(10, 2)
