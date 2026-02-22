d800043dfc2e19f75bb8ff99bc22f5c01d24aece
Enrico Nunes <enriconunes02@hotmail.com>
2026-02-22 16:26:29 +0000

    criando pull request com erro proposital

--- dividir.py
+++ dividir.py
@@ -1,8 +1,11 @@
 def dividir(numerador, denominador):
     if denominador == 0:
         raise ZeroDivisionError("division by zero")
-    return numerador / denominador  
+    return numerador * denominador  
 
 if __name__ == "__main__":
     resultado = dividir(10, 2)
     print("Resultado:", resultado)
+
+
+
